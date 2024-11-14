4e75238c61ee9cf3cd2e31da17512f40a03d06b2
davidrotaborudea <davids.rodriguez@udea.edu.co>
2024-11-14 11:10:19 -0500

    FTR deploy vercel

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,10 +6,6 @@ use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\URL;
 
-if (env('APP_ENV') == 'production') {
-    URL::forceScheme('https');
-}
-
 class AppServiceProvider extends ServiceProvider
 {
 
